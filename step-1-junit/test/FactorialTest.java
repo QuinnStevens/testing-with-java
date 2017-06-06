4e74bd3c6cb9c65c8d143fafb3f0099e5bbe24ff
Quinn Stevens <quinn.stevens@adacollege.org.uk>
2017-06-06 15:36:16 +0100

    Added 2 more tests, added @Before

--- step-1-junit/test/FactorialTest.java
+++ step-1-junit/test/FactorialTest.java
@@ -1,10 +1,17 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.Before;
 
 public class FactorialTest {
+	private Factorial factorial;
+
+	@Before
+	public void setUp() {
+		factorial = new Factorial();
+	}
+	
 	@Test
 	public void calculate_0() {
-		Factorial factorial = new Factorial();
 		int testStartValue = 0;
 		int expectedResult = 0;
 
@@ -13,10 +20,22 @@ public class FactorialTest {
 
 	@Test
 	public void calculate_1() {
-		Factorial factorial = new Factorial();
 		int testStartValue = 1;
 		int expectedResult = 1;
 
 		assertEquals(expectedResult, factorial.calculate(testStartValue));
+	}
+	
+	@Test
+	public void calculate_5() {
+		int testStartValue = 5;
+		int expectedResult = 120;
+
+		assertEquals(expectedResult, factorial.calculate(testStartValue));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void calculate_negative() {
+		factorial.calculate(-5);
 	}
 }
